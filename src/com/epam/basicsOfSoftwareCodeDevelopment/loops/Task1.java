49a8381e4902ffccc54390fe92d3ccdfcb7d3e0b
Denis <sm1l43s@yandex.ru>
2020-11-12 13:23:40 +0300

    Completed task 1

--- src/com/epam/basicsOfSoftwareCodeDevelopment/loops/Task1.java
+++ src/com/epam/basicsOfSoftwareCodeDevelopment/loops/Task1.java
@@ -1,4 +1,22 @@
 package com.epam.basicsOfSoftwareCodeDevelopment.loops;
 
+import java.util.Scanner;
+
 public class Task1 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input please any positiv number: ");
+        int number = in.nextInt();
+
+        int sum = 0;
+        for (int i = 0; i < number; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of the numbers in range 1-" + number + " = " + sum);
+    }
+
 }
